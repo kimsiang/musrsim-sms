1509f98da6c48240df903a5ca8a5faf4d6ca08ef
TwinklyStar <18930143913@163.com>
2021-11-20 00:25:12 +0800

    Import changes from CHENCheng

---
+++ cmake-build-default/CMakeFiles/musrSim.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/musrSim.dir/musrSim.cc.o"
+  "CMakeFiles/musrSim.dir/src/F04ElementField.cc.o"
+  "CMakeFiles/musrSim.dir/src/F04FieldMessenger.cc.o"
+  "CMakeFiles/musrSim.dir/src/F04GlobalField.cc.o"
+  "CMakeFiles/musrSim.dir/src/FTFP_BERT.cc.o"
+  "CMakeFiles/musrSim.dir/src/G4EmExtraPhysics.cc.o"
+  "CMakeFiles/musrSim.dir/src/MuDecayChannel.cc.o"
+  "CMakeFiles/musrSim.dir/src/MuDecayChannelWithSpin.cc.o"
+  "CMakeFiles/musrSim.dir/src/meyer.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrDetectorConstruction.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrDetectorMessenger.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrErrorMessage.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrEventAction.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrMuEnergyLossLandau.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrMuFormation.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrMuScatter.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrMuonium.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrParameters.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrPhysicsList.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrPrimaryGeneratorMessenger.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrQuadrupole.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrRootOutput.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrRunAction.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrScintHit.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrScintSD.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrStackingAction.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrSteppingAction.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrSteppingVerbose.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrTabulatedElementField.cc.o"
+  "CMakeFiles/musrSim.dir/src/musrUniformField.cc.o"
+  "CMakeFiles/musrSim.dir/src/yields.cc.o"
+  "musrSim"
+  "musrSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/musrSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
